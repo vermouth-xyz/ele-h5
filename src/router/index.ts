ede8dadd00dc6b451ef29d82e5abad250f661e89
vermouth <vermouthzxy@gmail.com>
2023-03-12 17:50:19 +0800

    类型: 新增 描述: 增加路由相关

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,40 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import TabsView from '../views/tabs/TabsView.vue'
+import HomeView from '../views/tabs/home/HomeView.vue'
+import OrderView from '../views/tabs/order/OrderView.vue'
+import MeView from '../views/tabs/me/MeView.vue'
+// import ErrorView from '../views/ErrorView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      name: 'tabs',
+      redirect: '/home',
+      component: TabsView,
+      children: [
+        {
+          name: 'home',
+          path: '/home',
+          component: HomeView
+        },
+        {
+          name: 'order',
+          path: '/order',
+          component: OrderView
+        },
+        {
+          name: 'me',
+          path: '/me',
+          component: MeView
+        }
+      ]
     }
+    // {
+    //   path: '*',
+    //   name: '404',
+    //   component: ErrorView
+    // }
   ]
 })
 
---
+++ src/views/tabs/TabsView.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- <div class="center">{{ active }}</div> -->
+  <RouterView />
+  <Tabbar v-model="active">
+    <TabbarItem name="home" icon="home-o">首页</TabbarItem>
+    <TabbarItem name="order" icon="bars">订单</TabbarItem>
+    <TabbarItem name="me" icon="contact">我的</TabbarItem>
+  </Tabbar>
+</template>
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+import { useRoute, useRouter, RouterView } from 'vue-router'
+import { Tabbar, TabbarItem } from 'vant'
+const router = useRouter()
+const route = useRoute()
+const active = ref(route.name as string)
+console.log(active)
+watch(active, (nv) => {
+  router.push({ name: nv })
+})
+</script>
